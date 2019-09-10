4f041226cedf67ec48dfd0db32429ad5da45f5ab
Isaac Finkbeiner <IsaacFink10@protonmail.com>
2019-09-10 19:16:50 -0400

    Adds conditional code to banner so we can import it

--- banner.py
+++ banner.py
@@ -1,17 +1,18 @@
+
 def banner(title, author):
     title_length = len(title)
     byline = f"By {author}"
     byline_length = len(byline)
     banner_length = max(title_length, byline_length) + 8
-
     print("=" * banner_length)
     print(f"{title:^{banner_length}}")
     print(f"{byline:^{banner_length}}")
+    print("=" * banner_length)
+    print("")
 
-
-banner("BANNER", "Isaac Finkbeiner")
-
-name = input("What is your name?" )
-title = input("What is your title?" )
-print("")
-banner(title, name)
+if __name__ == "__main__":
+    banner("BANNER", "Isaac Finkbeiner")
+    name = input("What is your name?" )
+    title = input("What is your title?" )
+    print("")
+    banner(title, name)
