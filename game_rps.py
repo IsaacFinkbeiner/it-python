57547dbe02d7837c8032e694d8dc80b469b7987f
Isaac Finkbeiner <IsaacFink10@protonmail.com>
2019-09-17 13:46:01 -0400

    siege rps

--- game_rps.py
+++ game_rps.py
@@ -1,59 +1,72 @@
 import random
 from banner import banner
 
-banner("Rock Paper Scissors", "By Isaac Finkbeiner")
-print("We are going to play Rock, Paper, Scissors. The first to win Two out of Three rounds is the winner.")
-print("If a triangle = 5 sides, how many sides does a hotdog have?")
-player_score = 0
-cpu_score = 0
+yes = 0
+while yes == 0:
+    banner("Fuze Jäger Montange", "Isaac Finkbeiner, Ryan Anderson, and Blaise Brockway")
+    print("We are going to play Fuze, Jäger, Montange. The first to win Two out of Three rounds is the winner.")
 
-#create a list of play options
-t = ["", "Rock", "Paper", "Scissors"]
 
-#assign a random play to the computer
+    player_score = 0
+    cpu_score = 0
 
-#set player to False
-player = False
+    #create a list of play options
+    t = ["", "Fuze", "Jäger", "Montange"]
 
-while player_score < 2:
-    computer = random.randint(1,3)
-#set player to True
-    if cpu_score == 2:
+    #assign a random play to the computer
+
+    #set player to False
+    player = False
+
+    while player_score < 2:
+        computer = random.randint(1,3)
+    #set player to True
+        if cpu_score == 2:
+            print("")
+            print("Oh no! The computer won! Try again, you puny mortal! You must follow Turtle_Dude_8 on Twitch to repent for your sins and failures.")
+            break
         print("")
-        print("Oh no! The computer won! Try again, you puny mortal! You must follow Turtle_Dude_8 on Twitch to repent for your sins and failures.")
-        break
-    print("")
-    print(f"SCORE: {player_score} - {cpu_score}")
-    print("1. Rock")
-    print("2. Paper")
-    print("3. Scissors")
-    player = int(input("What is your choice? "))
-    if player == computer:
-        print("You have tied with the CPU!")
-    elif player == 1:
-        if computer == 2:
-            print(f"You lose! {t[computer]} covers {t[player]}")
-            cpu_score = cpu_score + 1
+        print(f"SCORE: {player_score} - {cpu_score}")
+        print("1. Fuze")
+        print("2. Jäger")
+        print("3. Montange")
+        player = int(input("What is your choice? "))
+        if player == computer:
+            print("You have traded with the CPU!")
+        elif player == 1:
+            if computer == 2:
+                print(f"You lose! {t[computer]} Countered {t[player]}'s Cluster Grenade")
+                cpu_score = cpu_score + 1
+            else:
+                print(f"You win! {t[player]} Teamkills {t[computer]}")
+                player_score = player_score + 1
+        elif player == 2:
+            if computer == 3:
+                print(f"You lose! {t[computer]} Absolutely Destroyed {t[player]}")
+                cpu_score = cpu_score + 1
+            else:
+                print(f"You win! {t[player]} Countered {t[computer]}'s Cluster Grenade")
+                player_score = player_score + 1
+        elif player == 3:
+            if computer == 1:
+                print(f"You lose... {t[computer]} Teamkills {t[player]}")
+                cpu_score = cpu_score + 1
+            else:
+                print(f"You win! {t[player]} Absolutely Destroyed {t[computer]}")
+                player_score = player_score + 1
         else:
-            print(f"You win! {t[player]} smashes {t[computer]}")
-            player_score = player_score + 1
-    elif player == 2:
-        if computer == 3:
-            print(f"You lose! {t[computer]} cut {t[player]}")
-            cpu_score = cpu_score + 1
-        else:
-            print(f"You win! {t[player]} covers {t[computer]}")
-            player_score = player_score + 1
-    elif player == 3:
-        if computer == 1:
-            print(f"You lose... {t[computer]} smashes {t[player]}")
-            cpu_score = cpu_score + 1
-        else:
-            print(f"You win! {t[player]} cut {t[computer]}")
-            player_score = player_score + 1
-    else:
-        print("Invalid, check spelling!")
+            print("Invalid, check spelling!")
 
-if player_score == 2:
-    print("")
-    print("Congratz!!! You won against the computer! Great job! Play again sometime! And Follow Turtle_Dude_8 on Twitch!")
+    if player_score == 2:
+        print("")
+        print("Congratz!!! You won against the computer! Great job! And Follow Turtle_Dude_8 on Twitch!")
+    play_again = "no"
+    while play_again == "no":
+        play_again = input("Play again? ")
+        if play_again == "yes":
+            print("cool")
+            computer_score = 0
+            player_score = 0
+        else:
+            print("SAY yes YOU UTTER FRIDGE")
+            play_again = "no"
